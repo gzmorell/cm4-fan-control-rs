05c2553ad966a45261f41836542d04afcab92387
Francisco Gonzalez <gzmorell@gmail.com>
2024-04-09 02:03:20 +0200

    Changed to async tokio

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,43 @@
-use std::{
-    f32::consts::PI,
-    io::ErrorKind,
-    sync::{Arc, Mutex},
-    time::Duration,
-};
-
-use once_cell::sync::Lazy;
 use rppal::i2c::I2c;
+use std::f32::consts::PI;
+use tokio::fs;
+use tokio::signal::unix::{signal, SignalKind};
+use tokio::sync::mpsc;
+use tokio::task;
+use tokio::time::{sleep, Duration};
+use tokio_util::sync::CancellationToken;
 
-// I2C data
-/// The bus number for fan control
-const I2C_BUS: u8 = 10;
-/// The slave address for fan control
-const I2C_SLA: u16 = 0x2f;
-/// The SMBUS command for writing or reading fan speed
-const I2C_CMD: u8 = 0x30;
-
-/// The PWM frequency that the PWM fan should operate at (for the Noctua A4x10)
-// const PWM_FREQUENCY: f64 = 25_000.0;
-/// [°C] temperature below which to stop the fan
+/// Temperature below which to stop the fan
 const OFF_TEMP: f32 = 40.0;
-/// [°C] temperature above which to start the fan
+/// Temperature above which to start the fan
 const MIN_TEMP: f32 = 45.0;
-/// [°C] temperature above which to start the fan at full speed
+/// Temperature above which to reach full fan speed
 const MAX_TEMP: f32 = 75.0;
 
-/// The speed (percentage) that the fan is off at
+/// The speed percentage that the fan is off at
 const FAN_OFF: f32 = 0.0;
-/// The speed (percentage) that the lowest setting of the fan should be
+/// The speed percentage for lowest fan speed
 const FAN_LOW: f32 = 0.1;
-/// The speed (percentage) that the max setting of the fan is
+/// The speed percentage for full fan speed
 const FAN_MAX: f32 = 1.0;
-/// The steps that the fan speed should increase per each degree that the
-/// temperature increase
+/// The slope of the fan speed vs temperature
 const FAN_GAIN: f32 = (FAN_MAX - FAN_LOW) / (MAX_TEMP - MIN_TEMP);
-/// The max speed setting for the fan
-const MAX_SPEED: u8 = 0xFF;
+/// The max speed setting
+const MAX_SPEED: f32 = 255.0;
 
-static FAN_SPEED: Lazy<Arc<Mutex<u8>>> = Lazy::new(|| Arc::new(Mutex::new(0)));
+/// I2c fan control bus
+const I2C_BUS: u8 = 10;
+/// I2c fan control slave address
+const I2C_SLA: u16 = 0x2f;
+/// I2c fan control speed command
+const I2C_CMD: u8 = 0x30;
 
-const UDEV_ERROR: &str = r#"
-Access to the i2c bus should not require root permission, but the user should be in the i2c group.
-"#;
+/// Number of seconds between fan speed updates
+const UPDATE_PERIOD: u64 = 5;
 
-// const PWM_PERMISSION_ERROR: &str = r#"
-// By default, both channels are disabled.
+struct Measure(f32, u8);
 
-// To enable only PWM0 on its default pin (BCM GPIO 18, physical pin 12), add dtoverlay=pwm to /boot/config.txt on Raspberry Pi OS or boot/firmware/usercfg.txt on Ubuntu.
-// If you need both PWM channels, replace pwm with pwm-2chan, which enables PWM0 on BCM GPIO 18 (physical pin 12), and PWM1 on BCM GPIO 19 (physical pin 35).
-// More details on enabling and configuring PWM on other GPIO pins than the default ones can be found in /boot/overlays/README.
-// "#;
-
-// const GPIO_PERMISSION_ERROR: &str = r#"
-// In recent releases of Raspberry Pi OS (December 2017 or later), users that are part of the gpio group (like the default pi user) can access /dev/gpiomem and /dev/gpiochipN (N = 0-2) without needing additional permissions.
-// Either the current user isn’t a member of the gpio group, or your Raspberry Pi OS distribution isn't up-to-date and doesn't automatically configure permissions for the above-mentioned files.
-// Updating Raspberry Pi OS to the latest release should fix any permission issues.
-// Alternatively, although not recommended, you can run your application with superuser privileges by using sudo.
-
-// If you’re unable to update Raspberry Pi OS and its packages (namely raspberrypi-sys-mods) to the latest available release, or updating hasn't fixed the issue, you might be able to manually update your udev rules to set the appropriate permissions.
-// More information can be found at https://github.com/raspberrypi/linux/issues/1225 and https://github.com/raspberrypi/linux/issues/2289.
-// "#;
-
-/// Returns the temperature of the CPU in degrees Celsius.
-fn get_cpu_temp() -> f32 {
-    let temp_unparsed = match std::fs::read_to_string("/sys/class/thermal/thermal_zone0/temp") {
-        Ok(temp) => temp,
-        Err(e) => match e.kind() {
-            ErrorKind::PermissionDenied => {
-                panic!("Failed to read /sys/class/thermal/thermal_zone0/temp.")
-            }
-            ErrorKind::NotFound => {
-                panic!("No temperature sensor found. Make sure you're running on a Raspberry Pi.")
-            }
-            _ => "45000".to_string(),
-        },
-    };
-    temp_unparsed.trim().parse::<f32>().unwrap_or(45000.0) / 1000.0
-}
-
-/// The custom fan curve that determines the speed that the fan should be at
-/// based on the temperature reported back by the Raspberry Pi
+/// The fan percentage curve
 #[inline]
 fn fan_curve(temp: f32) -> f32 {
     (0.5 * (1.0 - ((PI * temp) / 50.0).sin())
@@ -87,53 +45,91 @@ fn fan_curve(temp: f32) -> f32 {
         / 2.0
 }
 
-/// Returns the fan speed as a value between 0x00 and 0xFF (MAX_SPEED)
-fn handle_fan_speed(cpu_temp: f32, i2c: &mut I2c) -> Result<u8, rppal::i2c::Error> {
+/// The fan speed vs temperature
+#[inline]
+fn fan_speed(cpu_temp: f32) -> u8 {
     let fan_percentage = match cpu_temp {
         t if t < OFF_TEMP => FAN_OFF,
         t if t < MIN_TEMP => FAN_LOW,
         t if t < MAX_TEMP => fan_curve(t),
         _ => FAN_MAX,
     };
-    let fan_speed = (MAX_SPEED as f32 * fan_percentage) as u8;
-    let mut old_speed = FAN_SPEED.lock().unwrap();
-    if old_speed.clone() != fan_speed {
-        i2c.smbus_write_byte(I2C_CMD, fan_speed)?;
-        *old_speed = fan_speed;
-    }
-
-    Ok(fan_speed)
+    (MAX_SPEED * fan_percentage).floor() as u8
 }
 
-fn main() {
-    let mut i2c = match I2c::with_bus(I2C_BUS) {
-        Ok(bus) => bus,
-        Err(rppal::i2c::Error::Io(e)) => match e.kind() {
-            ErrorKind::PermissionDenied => {
-                eprintln!("");
-                std::process::exit(1);
-            }
-            ErrorKind::NotFound => {
-                eprintln!("");
-                std::process::exit(1);
-            }
-            _ => panic!("Error: {e}"),
-        },
-        Err(e) => panic!("Error: {e}"),
-    };
-    i2c.set_slave_address(I2C_SLA).unwrap();
-    let initial_speed = i2c.smbus_read_byte(I2C_CMD).unwrap();
+/// The temperature of the cpu in degrees Celsius
+async fn get_cpu_temp() -> Result<f32, std::io::Error> {
+    let temp_unparsed = fs::read_to_string("/sys/class/thermal/thermal_zone0/temp").await?;
+    Ok(temp_unparsed.trim().parse::<f32>().unwrap_or(45000.0) / 1000.0)
+}
 
-    {
-        let mut fan_speed = FAN_SPEED.lock().unwrap();
-        *fan_speed = initial_speed;
+/// Update fan speed each PERIOD seconds
+async fn fan_handle(tx: tokio::sync::mpsc::Sender<Measure>, cancel: CancellationToken) {
+    let mut last_speed: u8 = 0;
+    let bus = I2c::with_bus(I2C_BUS);
+    if bus.is_err() {
+        eprintln!("Unable to open I2c bus: {I2C_BUS}");
+        cancel.cancel();
+        return;
     }
-
+    let mut i2c = bus.unwrap();
+    if i2c.set_slave_address(I2C_SLA).is_err() {
+        eprintln!("Unable to set slave address {I2C_SLA} in I2c bus: {I2C_BUS}");
+        cancel.cancel();
+        return;
+    }
     loop {
-        let cpu_temp = get_cpu_temp();
-        let fan_speed = handle_fan_speed(cpu_temp, &mut i2c).expect("Error setting fan speed");
-        println!("CPU Temp: {cpu_temp:.2}°C, Fan Speed: {fan_speed}");
-
-        std::thread::sleep(Duration::from_secs(5));
+        tokio::select! {
+            _ = sleep(Duration::from_secs(UPDATE_PERIOD)) => {
+                if let Ok(temp) = get_cpu_temp().await {
+                    let new_speed = fan_speed(temp);
+                    if new_speed != last_speed {
+                        if i2c.smbus_write_byte(I2C_CMD, new_speed).is_err() {
+                            eprintln!("Unable to set fan speed on slave address {I2C_SLA} in I2c bus: {I2C_BUS}");
+                            cancel.cancel();
+                            return;
+                        }
+                        last_speed = new_speed;
+                        tx.send(Measure(temp, new_speed)).await.unwrap();
+                    }
+                } else {
+                    eprintln!("Missing cpu temperature measure!");
+                    cancel.cancel();
+                    break;
+                }
+            }
+            _ = cancel.cancelled() => {
+                println!("Fan control stopped.");
+                break;
+            }
+        }
     }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut sig = signal(SignalKind::terminate())?;
+    let (tx, mut rx) = mpsc::channel(1);
+    let cancel = CancellationToken::new();
+    let cloned_cancel = cancel.clone();
+    let job = task::spawn(fan_handle(tx, cloned_cancel));
+    loop {
+        tokio::select! {
+            _ = sig.recv() => {
+                println!("Service stopped.");
+                cancel.cancel();
+                break;
+                }
+            _ = cancel.cancelled() => {
+                eprintln!("Exit on error!");
+                break;
+            }
+            Some(Measure(temp, speed)) = rx.recv() => {
+                println!("Cpu Temp: {temp:.2}°C, Fan Speed: {speed}");
+
+            }
+        }
+    }
+    job.await.unwrap();
+    Ok(())
 }
